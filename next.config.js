13b267ef1bbf8542ac0c34fe8cde22645d317e79
Yakup Selim Uçar <71130842+yakupselimucar@users.noreply.github.com>
2024-07-01 23:34:37 +0300

    drive link format is updated

--- next.config.js
+++ next.config.js
@@ -1,7 +1,13 @@
 // next.config.js
-
 module.exports = {
     images: {
-        domains: ['drive.google.com'],
+        remotePatterns: [
+            {
+                protocol: 'https',
+                hostname: 'drive.google.com',
+                port: '',
+                pathname: '/**',
+            },
+        ],
     },
 }
